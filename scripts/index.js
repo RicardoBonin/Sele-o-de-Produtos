0cb515ae94d4ad8186aa86c3e3cb34a67633d6aa
Ricardo Bonin <ricardo@brittes.com.br>
2020-08-23 02:37:09 -0300

    consumindo Get API, Imprimindo os produtos na div list-product e alterando o endpoind do button more-products

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+function get(url) {
+  const listProduct = document.getElementById("list-products");
+  const buttonMoreProducts = document.getElementById("more-products");
+  axios
+    .get(`https://${url}`, {})
+    .then((res) => {
+      console.log(res.data.nextPage);
+      buttonMoreProducts.innerHTML = `<button id="more-products" onclick="get('${res.data.nextPage}')">Ainda
+      mais
+      produtos
+      aqui!</button>`;
+      res.data.products.map((product) => {
+        listProduct.innerHTML += `
+        <div class="product">
+        <img src="${product.image}" alt="${product.name}">
+        <div class="description">
+          <h1>${product.name}</h1>
+          <p>${product.description}</p>
+          <h2>De: R$${product.oldPrice}</h2>
+          <h1>Por:R$${product.price}</h1>
+          <h2>ou ${product.installments.count}x de R$${product.installments.value}0</h2>
+          <button>Comprar</button>
+        </div>
+      </div>
+        `;
+      });
+    })
+    .catch((err) => console.log(err));
+}
+get("frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1");
